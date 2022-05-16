216e43d77ebc9a0d360afc35461ec6aff38ed073
Matheus Oliveira <matheusoliveira815@gmail.com>
2022-05-16 19:03:19 -0300

    commit inicial

---
+++ js/main.js
@@ -0,0 +1,41 @@
+jQuery(document).ready(function($) {
+    
+    //Realizar a fixação do header
+    window.onscroll = function() {
+        if(window.pageYOffset > 90){
+            $('#header').addClass("active");
+        }
+        else{
+            $('#header').removeClass("active");
+        }
+    };
+
+    //ISOTOPE
+    let btns = $("#galeria .button-group button");
+    btns.click(function(e){
+        $("#galeria .button-group button").removeClass("active")
+        e.target.classList.add("active");
+
+        let selector = $(e.target).attr("data-filter");
+        $("#galeria .grid").isotope({
+            filter: selector,
+        });
+    });
+
+    $(window).on("load", function(){
+        $("#galeria .grid").isotope({
+            filter: "*",
+        });
+    });
+
+    //MAGNIFIC POPUP (IMAGENS DA GALERIA)
+    $(".grid .popup-link").magnificPopup({
+        type: "image",
+        gallery: {
+            enabled: true,
+            tPrev: "Anterior",
+            tNext: "Próxima",
+            tCounter: "%curr% de %total%",
+        }
+    })
+});
