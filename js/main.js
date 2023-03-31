a30f4622653bb86a07c2d7d064952b8d06db88e9
Matheus Oliveira <matheusoliveira815@gmail.com>
2023-03-31 16:39:42 -0300

    Atualizaçao responsiva e layout

--- js/main.js
+++ js/main.js
@@ -3,10 +3,18 @@ jQuery(document).ready(function($) {
     //Realizar a fixação do header
     window.onscroll = function() {
         if(window.pageYOffset > 250){
-            $('#header').addClass("active");
+            $('#header').addClass("active"); //BACKGROUND HEADER
+            $('.navbar-brand').addClass("active-logo"); //LOGO DO SITE
+            $('.nav-link').addClass("active-nav"); //LINKS NAVBAR
+            $('.wpp-button').addClass("active-btn"); //CONTATO BUTTON
+            $('.menu-toggle').addClass("active-menu-toggle"); //TOGGLE BUTTON
         }
         else{
             $('#header').removeClass("active");
+            $('.navbar-brand').removeClass("active-logo"); //LOGO DO SITE
+            $('.nav-link').removeClass("active-nav");
+            $('.wpp-button').removeClass("active-btn");
+            $('.menu-toggle').removeClass("active-menu-toggle");
         }
     };
 
@@ -39,3 +47,19 @@ jQuery(document).ready(function($) {
         }
     })
 });
+
+//Função que vai adicionar e remover a visualização da sessão
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      console.log(entry)
+      if(entry.isIntersecting){
+        entry.target.classList.add('show');
+      } else{
+        entry.target.classList.remove('show');
+      }
+    });
+});
+
+
+const animacao = document.querySelectorAll('.fade-animated-right , .fade-animated-left, .fade-animated-in');
+animacao.forEach((el)  =>  observer.observe(el));
